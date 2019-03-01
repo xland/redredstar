5252993e370f2e5a1238810a705743d9e5ce206a
liulun <412588801@qq.com>
2019-03-01 21:47:27 +0800

    sqlite3_ing...

--- src/renderer/utils/compatible.js
+++ src/renderer/utils/compatible.js
@@ -12,42 +12,97 @@ const knex = require('knex')({
     }
 });
 
-let getArticleData = function () {
-    var aStr = fs.readFileSync(path.join(basePath, "a.data"), readConfig);
-    var aObj = JSON.parse(aStr);
-}
-let getTagData = function () {
-    var tStr = fs.readFileSync(path.join(basePath, "t.data"), readConfig);
-    var tObj = JSON.parse(tStr);
-}
-let getUserData = function () {
-    var uStr = fs.readFileSync(path.join(basePath, "u.data"), readConfig);
-    var uObj = JSON.parse(uStr);
-    knex.schema.createTable('settings', (table) => {
-        table.increments('id');
-        table.integer('autosave_interval');
-        table.integer('img_w');
-        table.integer('img_h');
-    }).then(function () {
-        return knex.insert({
-            autosave_interval: uObj.autoSaveIntervalSeconds,
-            img_w: 1300,
-            img_h: 800
-        }).into('settings');
-    })
-}
-let getOldData = function () {
-    if (fs.existsSync(path.join(basePath, "a.data"))) {
-        getArticleData();
-        //todo del old data
+export default {
+    basePath,
+    knex,
+    getArticleData(articles) {
+        knex.schema.createTable('articles', function (table) {
+            table.increments('id');
+            table.string('title');
+            table.integer('tab_index');
+            table.integer('is_selected');
+            table.bigInteger('temp_id');
+            table.timestamps();
+        }).then(function () {
+            let arr = articles.map(v => {
+                return {
+                    title: v.title,
+                    tab_index: -1,
+                    is_selected: 0,
+                    temp_id: v.id
+                }
+            })
+            return knex("articles").insert(arr);
+        });
+    },
+    getTagData(tags) {
+        knex.schema.createTable('tags', function (table) {
+            table.increments('id');
+            table.string('title');
+            table.bigInteger('temp_id');
+            table.timestamps();
+        }).then(function () {
+            let arr = tags.map(v => {
+                return {
+                    title: v.text,
+                    temp_id: v.id
+                }
+            })
+            return knex("tags").insert(arr);
+        });
+    },
+    getUserData(obj) {
+        knex.schema.createTable('settings', (table) => {
+            table.increments('id');
+            table.integer('autosave_interval');
+            table.integer('img_w');
+            table.integer('img_h');
+            table.timestamps();
+        }).then(function () {
+            return knex('settings').insert(obj);
+        })
+
+        knex.schema.createTable('article_tag', function (table) {
+            table.increments('id');
+            table.integer('tag_id');
+            table.integer('article_id');
+            table.timestamps();
+        }).then(function () {
+            return null
+        });
+    },
+    init() {
+        if (fs.existsSync(path.join(basePath, "a.data"))) {
+            var aStr = fs.readFileSync(path.join(basePath, "a.data"), readConfig);
+            var aObj = JSON.parse(aStr);
+            this.getArticleData(aObj);
+            //todo del old data
+        }else{
+            this.getArticleData([]);
+        }
+        if (fs.existsSync(path.join(basePath, "t.data"))) {
+            var tStr = fs.readFileSync(path.join(basePath, "t.data"), readConfig);
+            var tObj = JSON.parse(tStr);
+            this.getTagData(tObj);
+            //todo del old data
+        } else {
+            this.getTagData([]);
+        }
+        if (fs.existsSync(path.join(basePath, "u.data"))) {
+            var uStr = fs.readFileSync(path.join(basePath, "u.data"), readConfig);
+            var uObj = JSON.parse(uStr);
+            this.getUserData({
+                autosave_interval: uObj.autoSaveIntervalSeconds,
+                img_w: 1300,
+                img_h: 800
+            });
+            //todo del old data
+        } else {
+            this.getUserData({
+                autosave_interval: 8,
+                img_w: 1300,
+                img_h: 800
+            })
+        }
     }
-    if (fs.existsSync(path.join(basePath, "t.data"))) {
-        getTagData();
-        //todo del old data
-    }
-    if (fs.existsSync(path.join(basePath, "u.data"))) {
-        getUserData();
-        //todo del old data
-    }
 }
-getOldData();
