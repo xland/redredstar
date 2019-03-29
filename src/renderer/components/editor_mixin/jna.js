e9805e3a7413ed943d20090b62bbe43f8786c944
liulun <412588801@qq.com>
2019-03-29 22:03:03 +0800

    Update jna.js

--- src/renderer/components/editor_mixin/jna.js
+++ src/renderer/components/editor_mixin/jna.js
@@ -7,7 +7,9 @@ const {
 export default {
     data() {
         return {
-            jnaGard: 0
+            jnaGard: 0,
+            jnaUrl:'https://jiaonia.com',
+            //jnaUrl:'http://localhost:52081',
         }
     },
     methods: {
@@ -28,7 +30,7 @@ export default {
                 let imgFd = new FormData();
                 imgFd.append("token", this.$root.jnaToken);
                 imgFd.append("answerImg", file);
-                window.xxmPost("https://jiaonia.com/Xxm/PublishArticleImg", imgFd, rt => {
+                window.xxmPost(this.jnaUrl+"/Xxm/PublishArticleImg", imgFd, rt => {
                     this.jnaGard += 1;
                     var imgObj = JSON.parse(rt);
                     v.src = imgObj.url;
@@ -72,13 +74,13 @@ export default {
                     if (rows.length > 0) {
                         let eurlObj = JSON.parse(rows[0].edit_url);
                         fd.append("qId", eurlObj.qId);
-                        window.xxmPost("https://jiaonia.com/Xxm/EditArticle", fd, rt => {
+                        window.xxmPost(this.jnaUrl+"/Xxm/EditArticle", fd, rt => {
                             this.db("article_site").update({
                                 edit_url: rt
                             }).where("id", rows[0].id).then();
                         });
                     } else {
-                        window.xxmPost("https://jiaonia.com/Xxm/PublishArticle", fd, rt => {
+                        window.xxmPost(this.jnaUrl+"/Xxm/PublishArticle", fd, rt => {
                             this.db("article_site").insert({
                                 article_id: articleId,
                                 site_id: 'jiaonia',
