e8e62587c034af134ef0d42b4297620c2f586da6
liulun <412588801@qq.com>
2019-04-27 15:08:47 +0800

    mindmap add sub node cascade

--- src/renderer/components/mindnode.vue
+++ src/renderer/components/mindnode.vue
@@ -1,12 +1,13 @@
 <template>
-    <g :id="node.data.id" class="node" :transform="`translate(${node.data.x},${node.data.y})`">
+    <g :id="node.data.id" class="node" :transform="`translate(${leftValue},${node.data.y})`">
         <g @click="nodeSelect">
             <rect :class="[node.data.isSelected?'rectSelected':'']" width="100" height="30"></rect>
             <text stroke-width="0" fill="#fff" transform="translate(24,20)"
                 :id="node.data.id+'_t'">{{node.data.text||'[未命名]'}}</text>
             <!-- line -->
         </g>
-        <mindnode :key="item.data.id" :node="item" v-for="item in node.children"></mindnode>
+        <mindnode :key="item.data.id" :node="item" :isAtRight="item.data.x > 0" v-for="item in node.children">
+        </mindnode>
     </g>
 </template>
 <script>
@@ -16,19 +17,28 @@
         components: {
             mindnode
         },
-        props: ['node'],
+        props: ['node', 'isAtRight'],
         data() {
             return {
 
             }
         },
+        computed: {
+            leftValue() {
+                if (this.node.data.x > 0 && !this.isAtRight) {
+                    return 0 - this.node.data.x;
+                }
+                return this.node.data.x;
+            }
+        },
         methods: {
             nodeSelect() {
                 this.node.data.isSelected = !this.node.data.isSelected;
-                if(this.$parent.selectedNode){
-                    this.$parent.selectedNode.data.isSelected = false;
+                if (this.node.data.isSelected) {
+                    this.bus.$emit('resetSelectNode', this.node);
+                } else {
+                    this.bus.$emit('resetSelectNode', null);
                 }
-                this.$parent.selectedNode = this.node;
             }
         }
     }
--- src/renderer/views/mind.vue
+++ src/renderer/views/mind.vue
@@ -3,7 +3,7 @@
         <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1"
             xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs">
             <defs></defs>
-            <mindnode :node="mind.root">
+            <mindnode :node="mind.root" :isAtRight="true">
             </mindnode>
         </svg>
     </div>
@@ -45,8 +45,17 @@
             this.getData(id);
             this.centerRootNode();
             this.newNode();
+            this.resetSelectNode();
         },
         methods: {
+            resetSelectNode(){
+                this.bus.$on('resetSelectNode', (item) => {
+                    if(this.selectedNode){
+                        this.selectedNode.data.isSelected = false;
+                    }
+                    this.selectedNode = item;
+                });
+            },
             newNode() {
                 var self = this;
                 document.onkeydown = function (event) {
@@ -64,23 +73,40 @@
                         }
                         self.selectedNode.data.isSelected = false;
                         self.selectedNode.children.push(newNode);
-                        let count = self.selectedNode.children.length;
-                        let midIndex = Math.ceil(count / 2);
-                        let startY = 0 - ((midIndex - 1) * 60 + midIndex * 30) / 2 + 15;
-                        for (let i = 0; i < midIndex; i++) {
-                            let curNode = self.selectedNode.children[i];
-                            curNode.data.x = 200;
-                            curNode.data.y = startY + i * 60 + i * 30;
-                        }
-                        let leftCount = count - midIndex;
-                        startY = 0 - ((leftCount - 1) * 60 + leftCount * 30) / 2 + 15;
-                        for (let i = 0; i < leftCount; i++) {
-                            let curNode = self.selectedNode.children[midIndex + i];
-                            curNode.data.x = -200;
-                            curNode.data.y = startY + i * 60 + i * 30;
+                        if (self.selectedNode.data.isRoot) {
+                            self.setRootChild()
+                        } else {
+                            self.setNormalChild();
                         }
                         self.selectedNode = newNode;
                     }
+                }
+            },
+            setNormalChild() {
+                debugger;
+                let count = this.selectedNode.children.length;
+                let startY = 0 - ((count - 1) * 60 + count * 30) / 2 + 15;
+                for (let i = 0; i < count; i++) {
+                    let curNode = this.selectedNode.children[i];
+                    curNode.data.x = 200;
+                    curNode.data.y = startY + i * 60 + i * 30;
+                }
+            },
+            setRootChild() {
+                let count = this.selectedNode.children.length;
+                let midIndex = Math.ceil(count / 2);
+                let startY = 0 - ((midIndex - 1) * 60 + midIndex * 30) / 2 + 15;
+                for (let i = 0; i < midIndex; i++) {
+                    let curNode = this.selectedNode.children[i];
+                    curNode.data.x = 200;
+                    curNode.data.y = startY + i * 60 + i * 30;
+                }
+                let leftCount = count - midIndex;
+                startY = 0 - ((leftCount - 1) * 60 + leftCount * 30) / 2 + 15;
+                for (let i = 0; i < leftCount; i++) {
+                    let curNode = this.selectedNode.children[midIndex + i];
+                    curNode.data.x = -200;
+                    curNode.data.y = startY + i * 60 + i * 30;
                 }
             },
             centerRootNode() {
@@ -95,7 +121,9 @@
             },
             getData(id) {
                 this.mindPath = path.join(remote.app.getPath('userData'), "/xxm/m_" + id);
-                this.mind = JSON.parse(fs.readFileSync(path.join(this.mindPath, "m.data"), this.$root.rwOption));
+                let mind = fs.readFileSync(path.join(this.mindPath, "m.data"), this.$root.rwOption);
+                mind = JSON.parse(mind);
+                this.mind = mind;
                 this.db('minds').where("id", id).update({
                     "visited_at": new Date()
                 }).then(() => {
