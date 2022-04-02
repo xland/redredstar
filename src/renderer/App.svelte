b8331775ed0c347d3cf37403a2cc032b6ad72afd
liuxiaolun <412588801@qq.com>
2022-04-02 11:26:29 +0800

    title bar

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import TopBar from "./TopBar/TopBar.svelte";
   import { ipcRenderer } from "electron";
   import { onMount } from "svelte";
   import "./icon/iconfont.css";
@@ -8,32 +9,7 @@
   });
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-</main>
+<TopBar />
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+<style lang="scss">
 </style>
---
+++ src/renderer/TopBar/TopBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import WinTool from "./WinTool.svelte";
+</script>
+
+<div class="topBar">
+  <div class="winTitle">标题</div>
+  <WinTool />
+</div>
+
+<style lang="scss">
+  .topBar {
+    display: flex;
+    height: 38px;
+    line-height: 38px;
+    background: #eee;
+    width: 100%;
+  }
+  .winTitle {
+    flex: 1;
+  }
+</style>
