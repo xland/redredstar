fc65a33c24b12535c42f91c07ecd4bc0ae4a6d51
liulun <412588801@qq.com>
2022-04-03 20:58:24 +0800

    拖拽

--- src/renderer/Content/Category/CategoryTool.svelte
+++ src/renderer/Content/Category/CategoryTool.svelte
@@ -6,7 +6,7 @@
   .categoryTool {
     height: 38px;
     line-height: 38px;
-    padding-left: 12px;
+    padding-left: 8px;
     background: rgb(250, 250, 250);
     border-right: 1px solid rgb(240, 240, 240);
     border-bottom: 1px solid rgb(240, 240, 240);
--- src/renderer/Content/Content.svelte
+++ src/renderer/Content/Content.svelte
@@ -5,6 +5,44 @@
   import TitleList from './Title/TitleList.svelte'
   import Editor from './Editor.svelte'
   import { globalObjs } from '../Store/globalObjs'
+  import { onMount } from 'svelte'
+  let dragingDom: HTMLElement
+  let setCategoryWidth = (width = 180) => {
+    let categoryDom = document.querySelector('.category')
+    let titleListDom = document.querySelector('.titleList') as HTMLElement
+    let categorySplitter = document.getElementById('categorySplitter')
+    let titleSplitter = document.getElementById('titleSplitter')
+    categoryDom.setAttribute('style', `width:${width}px`)
+    categorySplitter.setAttribute('style', `left:${width - 3}px`)
+    titleSplitter.setAttribute('style', `left:${titleListDom.clientWidth + width - 3}px`)
+  }
+  let setTitleListWidth = (totalWidth = 500) => {
+    let titleListDom = document.querySelector('.titleList') as HTMLElement
+    let titleSplitter = document.getElementById('titleSplitter')
+    titleListDom.setAttribute('style', `width:${totalWidth - titleListDom.offsetLeft}px`)
+    titleSplitter.setAttribute('style', `left:${totalWidth - 3}px`)
+  }
+  let spliterDraging = (e: MouseEvent) => {
+    if (dragingDom.id === 'categorySplitter') {
+      setCategoryWidth(e.clientX)
+    } else {
+      setTitleListWidth(e.clientX)
+    }
+  }
+  let spliterDragEnd = () => {
+    document.removeEventListener('mousemove', spliterDraging)
+    document.removeEventListener('mouseup', spliterDragEnd)
+    dragingDom = null
+  }
+  let spliterMousedown = (e: MouseEvent) => {
+    document.addEventListener('mousemove', spliterDraging)
+    document.addEventListener('mouseup', spliterDragEnd)
+    dragingDom = e.target as HTMLElement
+  }
+  onMount(() => {
+    setCategoryWidth()
+    setTitleListWidth()
+  })
 </script>
 
 <div class="content">
@@ -12,25 +50,28 @@
     <CategoryTool />
     <CategoryTree />
   </div>
+  <div on:mousedown={spliterMousedown} id="categorySplitter" class="spliter" />
   <div class="titleList">
     <TitleTool />
     <TitleList />
   </div>
+  <div on:mousedown={spliterMousedown} id="titleSplitter" class="spliter" />
   <div on:click={() => globalObjs.editor.focus()} class="articleContent">
     <Editor />
   </div>
 </div>
 
 <style lang="scss">
-  .category {
-    width: 180px;
-    display: flex;
-    flex-direction: column;
-  }
   .content {
     display: flex;
     height: calc(100% - 68px);
     overflow: hidden;
+    position: relative;
+  }
+  .category {
+    width: 180px;
+    display: flex;
+    flex-direction: column;
   }
   .titleList {
     display: flex;
@@ -40,5 +81,15 @@
   .articleContent {
     flex: 1;
     cursor: text;
+  }
+  .spliter {
+    position: absolute;
+    z-index: 1;
+    width: 6px;
+    height: 100%;
+  }
+  .spliter:hover {
+    background: rgb(225, 228, 232);
+    cursor: w-resize;
   }
 </style>
